df299e7ec5ada7bfbf253aa3be37c59426cb7e75
Austin <joha0033@gmail.com>
2019-02-11 15:10:56 -0700

    Initial Commit

---
+++ chat-app-02-2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chat-app-02-2019
+//
+//  Created by WebLedDevelopment on 2/11/19.
+//  Copyright © 2019 WebLedDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
